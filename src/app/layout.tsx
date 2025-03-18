485dcd7da7082f5e5dfb5300c5163533f6ef2728
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-18 22:28:52 +0200

    chore: init the project

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '800'],
+});
+
+export const metadata: Metadata = {
+  title: 'Elevate',
+  description: 'Elevate Exam App',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Elevate exam app</h1>;
+}
